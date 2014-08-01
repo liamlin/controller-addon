46a27ccd7115a22a585380a713ba79238bb5ecb0
Wilsonlin <wilson@wilsonlin.me>
2014-08-01 11:35:14 -0700

    Implement QR code, and open and close tab

--- data/myjs.js
+++ data/myjs.js
@@ -1,25 +1,23 @@
 //var host = "http://localhost:8001";
 var host = "http://maccontroller.herokuapp.com";
 var socket = io.connect(host);
-//var uid = Math.random().toString(36).slice(2);
-//self.port.emit("uid", uid);
-//socket.emit("uid", uid);
-var uid = "5566";
+var uid = Math.random().toString(36).slice(2);
+
+self.postMessage({"uid": uid});
+
 socket.on(uid, function (data) {
     console.log(data);
     if (data.newTab) {
-        self.port.emit("newTab", data.newTab);
+        self.postMessage({"newTab": data.newTab});
     } else {
         var directive = parseInt(data);
         if (isNaN(directive))
             return;
         if (directive > 4) {
-//            self.port.emit("directive", directive);
-            self.postMessage(directive);
+            self.postMessage(self.postMessage({"toDo": directive}));
         } else
             $("a#"+directive).click();
     }
-//    socket.emit('result', true);
 });
 
 $(function(){
--- data/updateQRC.js
+++ data/updateQRC.js
@@ -1,4 +1,6 @@
-addon.port.on("updateQRC", function(data){
+self.port.on("updateQRC", function(data){
+    console.log("updateQRC");
+    console.log(data.src);
     img = document.getElementById("qrcode");
     img.src = data.src;
     uid_holder = document.getElementById("uid_holder");
